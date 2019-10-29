f8e8e82a2c312fcbe6b087b0c6926323e5ed6062
Amit Dash <amitdash291@gmail.com>
2019-10-30 04:12:10 +0530

    Cleanup code -handle nulls,  add constants

--- lib/src/main/java/me/ibrahimsn/lib/BottomBarParser.kt
+++ lib/src/main/java/me/ibrahimsn/lib/BottomBarParser.kt
@@ -4,7 +4,9 @@ import android.content.Context
 import android.content.res.XmlResourceParser
 import android.graphics.drawable.Drawable
 import androidx.core.content.ContextCompat
-import org.xmlpull.v1.XmlPullParserException
+import me.ibrahimsn.lib.Constants.ICON_ATTRIBUTE
+import me.ibrahimsn.lib.Constants.ITEM_TAG
+import me.ibrahimsn.lib.Constants.TITLE_ATTRIBUTE
 
 class BottomBarParser(private val context: Context, res: Int) {
 
@@ -15,7 +17,7 @@ class BottomBarParser(private val context: Context, res: Int) {
         var eventType: Int?
         do {
             eventType = parser.next()
-            if (eventType == XmlResourceParser.START_TAG && "item" == parser.name) {
+            if (eventType == XmlResourceParser.START_TAG && parser.name == ITEM_TAG) {
                 items.add(getTabConfig(parser))
             }
         } while (eventType != XmlResourceParser.END_DOCUMENT)
@@ -26,10 +28,12 @@ class BottomBarParser(private val context: Context, res: Int) {
         val attributeCount = parser.attributeCount
         var itemText: String? = null
         var itemDrawable: Drawable? = null
-        for (i in 0 until attributeCount) {
+
+        for (i in 0..attributeCount) {
             when (parser.getAttributeName(i)) {
-                "icon" -> itemDrawable = ContextCompat.getDrawable(context, parser.getAttributeResourceValue(i, 0))
-                "title" -> {
+                ICON_ATTRIBUTE -> itemDrawable =
+                    ContextCompat.getDrawable(context, parser.getAttributeResourceValue(i, 0))
+                TITLE_ATTRIBUTE -> {
                     itemText = try {
                         context.getString(parser.getAttributeResourceValue(i, 0))
                     } catch (e: Exception) {
@@ -38,6 +42,6 @@ class BottomBarParser(private val context: Context, res: Int) {
                 }
             }
         }
-        return BottomBarItem(itemText!!, itemDrawable!!, alpha = 0)
+        return BottomBarItem(itemText ?: "", itemDrawable!!, alpha = 0)
     }
 }
---
+++ lib/src/main/java/me/ibrahimsn/lib/Constants.kt
@@ -0,0 +1,8 @@
+package me.ibrahimsn.lib
+
+object Constants {
+    const val ITEM_TAG = "item"
+    const val ICON_ATTRIBUTE = "icon"
+    const val TITLE_ATTRIBUTE = "title"
+}
+
