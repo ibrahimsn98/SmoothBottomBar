5ba5cfbfd8e9021b083ac12152220f2ec67d53ed
Barış Söbe <baris.sobe@trendyol.com>
2019-11-08 18:12:53 +0300

    Add try catch block to handle notFoundException

--- lib/src/main/java/me/ibrahimsn/lib/BottomBarParser.kt
+++ lib/src/main/java/me/ibrahimsn/lib/BottomBarParser.kt
@@ -1,6 +1,7 @@
 package me.ibrahimsn.lib
 
 import android.content.Context
+import android.content.res.Resources
 import android.content.res.XmlResourceParser
 import android.graphics.drawable.Drawable
 import androidx.annotation.XmlRes
@@ -35,12 +36,16 @@ class BottomBarParser(private val context: Context, @XmlRes res: Int) {
         for (index in 0 until attributeCount) {
             when (parser.getAttributeName(index)) {
                 ICON_ATTRIBUTE -> {
-                    itemDrawable = ContextCompat.getDrawable(context, parser.getAttributeResourceValue(index, 0))
+                    itemDrawable = try {
+                        ContextCompat.getDrawable(context, parser.getAttributeResourceValue(index, 0))
+                    } catch (notFoundException: Resources.NotFoundException) {
+                        null
+                    }
                 }
                 TITLE_ATTRIBUTE -> {
                     itemText = try {
                         context.getString(parser.getAttributeResourceValue(index, 0))
-                    } catch (e: Exception) {
+                    } catch (notFoundException: Resources.NotFoundException) {
                         parser.getAttributeValue(index)
                     }
                 }
