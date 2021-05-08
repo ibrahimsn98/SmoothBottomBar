fa29818e8277586b9c9ac942e21019504301e5fa
Mayokun Adeniyi <adeniyimayokun17@gmail.com>
2021-05-08 15:55:12 +0100

    fix: change smoothbottombar nav controller setup

--- app/src/main/java/me/ibrahimsn/smoothbottombar/MainActivity.kt
+++ app/src/main/java/me/ibrahimsn/smoothbottombar/MainActivity.kt
@@ -2,28 +2,64 @@ package me.ibrahimsn.smoothbottombar
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import android.widget.PopupMenu
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
-import kotlinx.android.synthetic.main.activity_main.*
+import me.ibrahimsn.smoothbottombar.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
+class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         navController = findNavController(R.id.main_fragment)
         setupActionBarWithNavController(navController)
+        setupSmoothBottomMenu()
     }
 
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_bottom,menu)
-        bottomBar.setupWithNavController(menu!!,navController)
+        menuInflater.inflate(R.menu.another_menu, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.another_item_1 -> {
+                showToast("Another Menu Item 1 Selected")
+            }
+
+            R.id.another_item_2 -> {
+                showToast("Another Menu Item 2 Selected")
+            }
+
+            R.id.another_item_3 -> {
+                showToast("Another Menu Item 3 Selected")
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
+    private fun showToast(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun setupSmoothBottomMenu() {
+        val popupMenu = PopupMenu(this, null)
+        popupMenu.inflate(R.menu.menu_bottom)
+        val menu = popupMenu.menu
+        binding.bottomBar.setupWithNavController(menu, navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        navController.navigateUp()
-        return true
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
